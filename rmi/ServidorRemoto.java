da8a6dc2c7fa429a2743f139405ec91470ccfa5a
Ivan Pinheiro <Ivanppf1@gmail.com>
2025-01-22 11:12:01 -0300

    FEAT working server-client setup

---
+++ rmi/ServidorRemoto.java
@@ -0,0 +1,11 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+
+public interface ServidorRemoto extends Remote{
+
+	public void escreveMsg (String msg)throws RemoteException;
+		
+	public Date dataDeHoje() throws RemoteException;
+}
